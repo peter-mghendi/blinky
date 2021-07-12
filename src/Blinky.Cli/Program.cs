c7142b7c1338e1746d35c957d0dcfd289bbdad70
sixpeteunder <plenjo15@gmail.com>
2021-07-12 07:41:27 +0300

    Blink LED

--- src/Blinky.Cli/Program.cs
+++ src/Blinky.Cli/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Device.Gpio;
+using System.Threading;
 
-namespace Blinky.Cli
+const int pin = 18;
+
+const int lightTime = 1000;
+const int dimTime = 200;
+
+using GpioController controller = new();
+controller.OpenPin(pin, PinMode.Output);
+Console.WriteLine($"GPIO pin enabled: {pin}");
+
+while (true)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+    Console.WriteLine($"Light for {lightTime}ms");
+    controller.Write(pin, PinValue.High);
+    Thread.Sleep(lightTime);
+
+    Console.WriteLine($"Dim for {dimTime}ms");
+    controller.Write(pin, PinValue.Low);
+    Thread.Sleep(dimTime);
 }
